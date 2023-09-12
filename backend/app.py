08617bd40a1510f5426fee0e293104d45c6af390
Kamila <kamila8520@gmail.com>
2023-09-12 23:13:26 +0200

    add docstrings

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,18 @@ api = Flask(__name__)
 
 @api.route('/set_config', methods=['POST'])
 def save_config():
+    """
+    Save configuration data to a JSON file.
+
+    This route handles HTTP POST requests to save configuration data received in JSON format
+    to a 'config.json' file. The configuration data is expected to be in the request body.
+
+    Returns
+    -------
+    dict
+        A dictionary containing a success message indicating that the configuration data
+        has been saved.
+    """
     with open('config.json', 'w', encoding='utf-8') as f:
         json.dump(request.json, f, ensure_ascii=False, indent=4)
     return {'message': 'config saved'}
@@ -17,12 +29,45 @@ def save_config():
 
 @api.route('/init_conv', methods=['POST'])
 def init_conversation():
+    """
+    Initialize a conversation by copying an initial chat history.
+
+    This route handles HTTP POST requests to initialize a conversation by copying
+    an initial chat history file ('chat_init.json') to the current chat history file
+    ('chat_history.json'). The copied chat history serves as the starting point for
+    the conversation as it contains a system message and some examples that the chatbot
+    should follow.
+
+    Returns
+    -------
+    dict
+        A dictionary containing a success message indicating that the conversation
+        has been initialized.
+    """
     shutil.copyfile('chat_init.json', 'chat_history.json')
     return {'message': 'conversation initialized'}
 
 
 @api.route('/save_data', methods=['POST', 'GET'])
 def save_and_respond():
+    """
+    Process user input, generate responses, and provide correction information.
+
+    This route handles HTTP POST and GET requests to process user input and provide responses.
+    It expects a JSON object in the request body containing a user message ('message'). The
+    route then generates a response using the 'get_response' function and provides correction
+    information using the 'correct_user_message' function.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the following elements:
+        - 'message': The chatbot's response to the user's message.
+        - 'correction': A list of corrected text segments (if available), or an error message.
+        - 'format': A list of formatting indicators for each text segment (if available), or
+          'no-feedback' if no correction was provided.
+
+    """
     data = request.json
     correction, chatbot_message = get_response(data['message'])
     if correction is None:
--- backend/chat.py
+++ backend/chat.py
@@ -5,6 +5,28 @@ from difflib import Differ
 
 
 def get_response(user_message):
+    """
+    Generate a response from a chatbot using OpenAI's GPT-3.5 Turbo model.
+
+    This function takes a user's message as input, adds it to the chat history,
+    and retrieves a response from the chatbot model. It also handles the splitting
+    of the chatbot's response if it contains a correction.
+
+    Parameters
+    ----------
+    user_message : str
+        The user's message to be included in the conversation.
+
+    Returns
+    -------
+    tuple
+        A tuple containing two elements:
+        correction : str or None
+            If the chatbot's response contains a correction, this will be the corrected message.
+            Otherwise, it will be None.
+        chatbot_message : str
+            The chatbot's response to the user's message.
+    """
     with open('config.json', 'r', encoding='utf-8') as conf, \
          open('chat_history.json', 'r', encoding='utf-8') as conv:
         config = json.load(conf)
@@ -21,11 +43,11 @@ def get_response(user_message):
     chatbot_response = dict(response['choices'][0]['message'])
     chat_history.append(chatbot_response)
 
-    with open('chat_history.json', 'w', encoding='utf-8') as f:
-        json.dump(chat_history, f, ensure_ascii=False, indent=4)
+    with open('chat_history.json', 'w', encoding='utf-8') as conv:
+        json.dump(chat_history, conv, ensure_ascii=False, indent=4)
 
     if '\n---\n' in chatbot_response['content']:
-        correction, chatbot_message = chatbot_response['content'].split('\n---\n')
+        correction, chatbot_message = chatbot_response['content'].split('\n---\n', 1)
     else:
         correction = None
         chatbot_message = chatbot_response['content']
@@ -34,22 +56,43 @@ def get_response(user_message):
 
 
 def correct_user_message(user_txt, correct_txt):
+    """
+    Compare two texts and generate formatted corrections.
+
+    Parameters
+    ----------
+    user_txt : str
+        The user's input text to be compared for corrections.
+    correct_txt : str
+        The corrected text to compare against.
+
+    Returns
+    -------
+    tuple
+        A tuple containing two lists:
+        text_concat : list of str
+            A list of corrected text segments, where corrections are applied.
+        formatting_concat : list of str
+            A list of formatting indicators for each text segment, where formatting
+            types are 'normal', 'error', or 'correction'.
+
+    Notes
+    -----
+    This function compares two input texts using the difflib.Differ class to find
+    the differences between the two texts. It then extracts the corrected segments
+    and their associated formatting types ('normal', 'error', or 'correction').
+    """
     difference = Differ().compare(user_txt.split(), correct_txt.split())
     format_type = {' ': 'normal', '-': 'error', '+': 'correction'}
 
     text, formatting = [], []
-    for word in difference:
-        if word[0] != '?':
-            text.append(word[2:])
-            formatting.append(format_type[word[0]])
+    for diff in difference:
+        if diff[0] in format_type:
+            tag, word = format_type[diff[0]], diff[2:]
+            if len(formatting) != 0 and tag == formatting[-1]:
+                text[-1] += ' ' + word
+            else:
+                text.append(word)
+                formatting.append(tag)
 
-    start = 0
-    text_concat, formatting_concat = [], []
-    formatting.append('end')
-    for i in range(1, len(formatting) + 1):
-        if len(set(formatting[start:i])) != 1:
-            text_concat.append(' '.join(text[start:(i-1)]))
-            formatting_concat.append(formatting[start])
-            start = i - 1
-
-    return text_concat, formatting_concat
+    return text, formatting
